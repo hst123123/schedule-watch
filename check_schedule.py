7e45522e1e2b9744c8e70e0452b1669e55f605e6
hst123123 <howsiong@gmail.com>
2026-09-12 10:14:22 +0900

    Add files via upload

--- check_schedule.py
+++ check_schedule.py
@@ -20,7 +20,7 @@ from bs4 import BeautifulSoup
 # ---- Configuration (edit this list to add/remove pages to watch) ---------
 URLS = [
     "https://amaterasu-yokohama.com/therapist/43069",
-    "https://amaterasu-yokohama.com/schedule?day=2026-09-14&from=2026-09-14",
+    "https://amaterasu-yokohama.com/schedule?day=2026-09-19&from=2026-09-14",
 ]
 NTFY_TOPIC = os.environ.get("NTFY_TOPIC", "")  # set this to your unique topic name
 STATE_FILE = Path(os.environ.get("STATE_FILE", "state.json"))
